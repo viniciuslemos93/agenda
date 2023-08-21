c7ac51a6d9c44f1cea1bfa86e1d7a2c2c1bc876c
viniciuslemos93 <viniciuslemos.dev@gmail.com>
2023-08-20 23:19:36 -0300

    feat: Criando os templates de header e footer

--- create.php
+++ create.php
@@ -2,9 +2,7 @@
     include_once("templates/header.php");
 ?>
 
-    <h1>Testando Agenda</h1>
-    <i class="fas fa-eye"></i>
-
+    <h1>Criando Contato</h1>    
 <?php
     include_once("templates/footer.php");
 ?>
