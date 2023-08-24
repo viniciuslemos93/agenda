c8ecb626c98f28e3ac14accbe0c64507fe2220e6
viniciuslemos93 <viniciuslemos.dev@gmail.com>
2023-08-24 08:01:12 -0300

    feat: Componetização do botão de voltar

--- show.php
+++ show.php
@@ -2,7 +2,8 @@
     include_once("templates/header.php");
 ?>
     <div class="container" id="view-contact-container">
-        <h1 id="main-title"><?= $contact["name"]?></h1>
+        <?php include_once("templates/backbtn.html") ?>
+        <h1 id="main-title" class="main-title"><?= $contact["name"]?></h1>
         <p class="bold">Telefone: </p>
         <p><?= $contact["phone"]?></p>
         <p class="bold">Observações: </p>
