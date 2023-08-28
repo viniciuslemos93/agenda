b8e957e90b4024c62f5820b1194dfc13ef3b92b7
viniciuslemos93 <viniciuslemos.dev@gmail.com>
2023-08-27 23:14:04 -0300

    feat: Funcionalidade de remoção de contatos

--- config/process.php
+++ config/process.php
@@ -63,6 +63,27 @@
                 echo "Erro: $error";
             }
 
+        } else if ($data["type"] === "delete") {
+
+            $id = $data["id"];
+
+            $query = "DELETE FROM contacts WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+
+            $stmt->bindParam(":id", $id);            
+
+            try {
+
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato removido com sucesso!";
+
+            } catch (PDOExeption $e) {
+                //Erro na conexão
+                $error = $e -> getMessage();
+                echo "Erro: $error";
+            }
+
         }
 
         // REDIRECT HOME - APÓS A CRIAÇÃO DO CONTATO
